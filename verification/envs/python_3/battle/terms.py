8bce5237c16408df1b1e0bf0404184833999dfb1
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-05-22 07:48:31 +0300

    move terms from commander

---
+++ verification/envs/python_3/battle/terms.py
@@ -0,0 +1,18 @@
+__all__ = ["ROLE", "PARTY"]
+
+
+class PARTY():
+    REQUEST_NAME = 'parties'
+    MY = "my"
+    ENEMY = "enemy"
+    ALL = (MY, ENEMY)
+
+
+class ROLE():
+    REQUEST_NAME = "roles"
+    CENTER = "center"
+    TOWER = "tower"
+    UNIT = "unit"
+    BUILDING = 'building'
+    OBSTACLE = "obstacle"
+    ALL = (CENTER, TOWER, UNIT, BUILDING, OBSTACLE)
