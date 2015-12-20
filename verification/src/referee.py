f80a13867d978da01128e33af470e157daa6436d
Oleksandr Liabakh <oduvan@Oleksandrs-MacBook-Pro-2.local>
2015-12-20 16:54:33 +0200

    4 battles issues

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -578,8 +578,8 @@ class FightHandler(BaseHandler):
     def generate_craft_place(self):
         width = self.map_size[1]
         craft_positions = [cr.coordinates[1] for cr in self.crafts.values()]
-        available = [y for y in range(6, width - 6)
-                     if not any(pos - 5 <= y <= pos + 5 for pos in craft_positions)]
+        available = [y for y in range(3, width - 3)
+                     if not any(pos - 2 <= y <= pos + 2 for pos in craft_positions)]
         return [self.map_size[0], choice(available) if available else 0]
 
     def all_crafts_empty(self):
