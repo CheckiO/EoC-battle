c9e37acc90d9438319a4e49e7cdbbc1dd2a35c86
zeratulayuris <amk86_yuki@mail.ru>
2016-02-20 10:51:38 +0500

    Update terms.py

--- verification/envs/python_3/battle/terms.py
+++ verification/envs/python_3/battle/terms.py
@@ -1,4 +1,4 @@
-__all__ = ["ROLE", "PARTY"]
+__all__ = ["ROLE", "PARTY", "ITEM_TYPE"]
 
 
 class PARTY():
@@ -16,3 +16,13 @@ class ROLE():
     BUILDING = 'building'
     OBSTACLE = "obstacle"
     ALL = (CENTER, TOWER, UNIT, BUILDING, OBSTACLE)
+
+class ITEM_TYPE():
+    REQUEST_NAME = "types"
+    ROCKETBOT = "rocketBot"
+    INFANTRYBOT = "infantryBot"
+    HEAVYBOT = "heavyBot"
+    SENTRYGUN = "sentryGun"
+    MACHINEGUN = "machineGun"
+    ROCKETGUN = "rocketGun" #needs checking
+    ALL = (ROCKETBOT, INFANTRYBOT, HEAVYBOT, SENTRYGUN, MACHINEGUN, ROCKETGUN)
