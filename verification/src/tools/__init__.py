82488cc8e9e9c404229ca61ceb29b445cc0ec279
Alexander Lyabah <a.lyabah@checkio.org>
2019-08-14 12:26:34 +0300

    rpdb

--- verification/src/tools/__init__.py
+++ verification/src/tools/__init__.py
@@ -2,3 +2,7 @@ from .grid import *
 from .precalculated import *
 from .distances import *
 from .terms import *
+
+def debug():
+    import rpdb
+    rpdb.Rpdb(addr='0.0.0.0', port=4444).set_trace()
